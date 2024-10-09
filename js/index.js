c8f7aa3cef36a9858a74b3a8244224e9bdde2843
SomenathPaul <somenathpaulbusiness10@gmail.com>
2024-10-10 00:50:11 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var responsive_nav = document.getElementsByClassName("responsive-nav");
+
+    function openNav(){
+        responsive_nav.style.display="block";
+    }
+    function closeNav(){
+        responsive_nav.style.display="none";
+    }
